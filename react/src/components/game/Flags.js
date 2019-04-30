059dceac00025790836d9a63dff0c55c3f70e233
antoine <antoine.gannat@gmail.com>
2019-04-30 16:00:33 -0700

    add default flag

--- react/src/components/game/Flags.js
+++ react/src/components/game/Flags.js
@@ -12,14 +12,18 @@ class Flags extends Component {
             { name: 'Germany', url: flags_path + "germany.png" },
             { name: 'Italy', url: flags_path + "italy.png" },
             { name: 'Russia', url: flags_path + "russia.png" },
-            { name: 'Turkey', url: flags_path + "turkey.png" }
+            { name: 'Turkey', url: flags_path + "turkey.png" },
+            { name: 'Unknown', url: flags_path + "unknown.png" }
         ];
     }
 
     render() {
+        var flag_name;
         if (!this.props.flag)
-            return ("Unknown flag");
-        return (<img className={this.props.className} src={this.flags.find((f) => { return (f.name === this.props.flag) }).url} alt={this.props.flag} title={this.props.flag} />);
+            flag_name = "Unknown";
+        else
+            flag_name = this.props.flag;
+        return (<img className={this.props.className} src={this.flags.find((f) => { return (f.name === flag_name) }).url} alt={flag_name} title={flag_name} />);
     }
 }
 
